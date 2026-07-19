3999d85296c8328363f051fdeddfabcfc1a0bd5b
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-19 10:25:47 +0300

    Added tmdb service

---
+++ services/tmdb.js
@@ -0,0 +1,22 @@
+const TMDB_URL = "https://api.themoviedb.org/3";
+
+const tmdbOptions = {
+    headers: {
+        Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+        accept: "application/json",
+    }
+};
+
+const getPopularMovies = async() => {
+    const response = await fetch(`${TMDB_URL}/movie/popular`, tmdbOptions);
+
+    if(!response.ok) res.send("Failed to fetch popular movies from TMDB");
+
+    const data = await response.json();
+
+    return data;
+}
+
+module.exports = {
+    getPopularMovies
+}
