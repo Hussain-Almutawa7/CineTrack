26643889d58d429dc0055396217a6239f44cd8e8
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-21 01:16:38 +0300

    Added a popup for review to utilize it for edit

---
+++ public/scripts/handle-dialog.js
@@ -0,0 +1,18 @@
+const dialogue = document.querySelector(".add-review");
+const openReview = document.querySelector(".open-add-review");
+const closeReview = document.querySelector(".close-review");
+const cancelReview = document.querySelector(".cancel-review");
+
+if (dialogue && openReview && closeReview && cancelReview) {
+    openReview.addEventListener("click", () => {
+        dialogue.showModal();
+    });
+
+    closeReview.addEventListener("click", () => {
+        dialogue.close();
+    });
+
+    cancelReview.addEventListener("click", () => {
+        dialogue.close();
+    });
+}
