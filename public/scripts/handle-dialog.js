3ce24aa97228297f40dbe24680fcedc63455fa74
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-21 02:49:59 +0300

    Completed edit review

--- public/scripts/handle-dialog.js
+++ public/scripts/handle-dialog.js
@@ -4,9 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const closeReview = document.querySelector(".close-review");
     const cancelReview = document.querySelector(".cancel-review");
 
+    const editDialogue = document.querySelector(".edit-review");
+    const openEditReview = document.querySelector(".open-edit-review");
+    const closeEditReview = document.querySelector(".close-edit-review");
+    const cancelEditReview = document.querySelector(".cancel-edit-review");
+
     if (dialogue && openReview && closeReview && cancelReview) {
         openReview.addEventListener("click", () => {
             dialogue.showModal();
+
         });
 
         closeReview.addEventListener("click", () => {
@@ -17,4 +23,20 @@ document.addEventListener("DOMContentLoaded", () => {
             dialogue.close();
         });
     }
+
+    if (editDialogue && openEditReview && closeEditReview && cancelEditReview) {
+        openEditReview.addEventListener("click", () => {
+            editDialogue.showModal();
+
+        });
+
+        closeEditReview.addEventListener("click", () => {
+            editDialogue.close();
+        });
+
+        cancelEditReview.addEventListener("click", () => {
+            editDialogue.close();
+        });
+    }
+
 });
