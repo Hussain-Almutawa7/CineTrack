e65ebf4cc3d012e951f18be583a77e56d9a4e147
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-21 12:58:49 +0300

    Added test create funcion

--- controllers/rate-controller.js
+++ controllers/rate-controller.js
@@ -3,7 +3,25 @@ const Media = require("../models/media");
 const User = require("../models/user");
 
 const saveRating = async (req, res) => {
+    let media = await Media.findOne({
+        tmdbId: Number(req.params.movieId),
+        mediaType: "movie",
+    });
 
+    if (!media) {
+        media = await Media.create({
+            tmdbId: Number(req.params.movieId),
+            mediaType: "movie"
+        });
+    }
+
+    await Rating.create({
+        user: req.session.user.id,
+        media: media._id,
+        rating: 10
+    });
+
+    res.redirect(`/movies/${req.params.movieId}`)
 }
 
 const deleteRating = async (req, res) => {
