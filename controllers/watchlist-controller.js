34b0ad8781ba4ecca5e81e34e480175b6564066f
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-22 03:33:38 +0300

    implemented Add to watchlist function

--- controllers/watchlist-controller.js
+++ controllers/watchlist-controller.js
@@ -2,11 +2,37 @@ const Media = require("../models/media");
 const User = require("../models/user");
 
 const showWatchlist = async (req, res) => {
+    const user = await User.findById(req.session.user.id).populate("watchlist");
 
+    res.render("watchlist.ejs", {
+        watchlist: user.watchlist
+    });
 }
 
 const addToWatchlist = async (req, res) => {
+    const user = await User.findById(req.session.user.id);
 
+    let media = await Media.findOne({
+        tmdbId: Number(req.params.mediaId),
+        mediaType: req.params.mediaType,
+    });
+
+    if (!media) {
+        media = await Media.create({
+            tmdbId: Number(req.params.mediaId),
+            mediaType: req.params.mediaType,
+        });
+    }
+
+    const exisitingMedia = user.watchlist.some(mediaId => mediaId.equals(media._id));
+
+    if (!exisitingMedia) {
+        user.watchlist.push(media._id);
+        await user.save();
+    }
+
+    res.redirect(`/movies/${req.params.mediaId}`)
+    // res.redirect(`/${req.params.mediaType}/${req.params.mediaId}`) I will make all the routes dynamic for better usability in the future
 }
 
 const removeFromWatchList = async (req, res) => {
