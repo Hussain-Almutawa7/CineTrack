b30972b29f88bb12f0ea67a34cb7a6af68260d9a
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-22 09:48:31 +0300

    Make media object to work on both tv and media under one attributes name

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -5,7 +5,7 @@ const Rating = require("../models/rating");
 
 const mediaDetails = async (req, res) => {
     try {
-        const movie = await tmdbService.getMediaDetails(req.params.mediaType, req.params.mediaId);
+        const tmdbMedia = await tmdbService.getMediaDetails(req.params.mediaType, req.params.mediaId);
 
         const media = await Media.findOne({
             tmdbId: Number(req.params.mediaId),
@@ -45,8 +45,20 @@ const mediaDetails = async (req, res) => {
             }
         }
 
+        const mediaTye = {
+            id: tmdbMedia.id,
+            title: req.params.mediaTye === "movie" ? tmdbMedia.title : tmdbMedia.name,
+            releaseDate: req.params.mediaType === "movie" ? tmdbMedia.release_date : tmdbMedia.first_air_date,
+            runtime: req.params.mediaTye === "movie" ? tmdbMedia.runtime : tmdbMedia.episode_run_time[0],
+            posterPath: tmdbMedia.poster_path,
+            overview: tmdbMedia.overview,
+            backdropPath: tmdbMedia.backdrop_path,
+            tagline: tmdbMedia.tagline,
+            genres: tmdbMedia.genres,
+        }
+
         res.render("media/movie-details.ejs", {
-            movie,
+            tmdbMedia : mediaTye,
             reviews,
             avgRating,
             userRating,
