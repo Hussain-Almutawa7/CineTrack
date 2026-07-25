468f1aa7a687bf6c98496bbc0bc01bd1c07ca91c
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-26 01:17:03 +0300

    Implemented add user logic for admin

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
 const Review = require("../models/review");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const showUsers = async (req, res) => {
     const users = await User.find();
@@ -12,7 +13,39 @@ const showUser = async (req, res) => {
 }
 
 const addUser = async (req, res) => {
-    
+    const userInDatabase = await User.findOne({
+        $or: [
+            {
+                username: req.body.username.trim(),
+            },
+            {
+                email: req.body.email.trim().toLowerCase(),
+            },
+        ]
+    });
+
+    if (userInDatabase) {
+        return res.status(409).render("error.ejs", {
+            statusCode: 409,
+            title: "Account Already Exists",
+            message: "The username or email you entered is already being used.",
+            returnLink: "/auth/sign-up",
+            returnText: "Return to Sign Up",
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const userData = {
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        role: req.body.role
+    }
+
+    await User.create(userData);
+
+    res.redirect("/");
 }
 
 const editUser = async (req, res) => {
