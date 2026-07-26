3eda4a42bc9e0259a64edef969d5f22274637fe6
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-26 09:13:15 +0300

    Added update user logic

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -45,10 +45,43 @@ const addUser = async (req, res) => {
 
     await User.create(userData);
 
-    res.redirect("/");
+    res.redirect("/admin/users");
 }
 
 const editUser = async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    
+    const userData = {
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        role: req.body.role
+    }
+
+    const userInDatabase = await User.findOne({
+        $or: [
+            {
+                username: req.body.username.trim(),
+            },
+            {
+                email: req.body.email.trim().toLowerCase(),
+            },
+        ]
+    });
+
+    if (!userInDatabase) {
+        return res.status(409).render("error.ejs", {
+            statusCode: 409,
+            title: "Account Already Exists",
+            message: "The username or email you entered is already being used.",
+            returnLink: "/auth/sign-up",
+            returnText: "Return to Sign Up",
+        });
+    }
+
+    await User.findOneAndUpdate(userInDatabase, userData);
+    res.redirect("/admin/users");
+
 
 }
 
